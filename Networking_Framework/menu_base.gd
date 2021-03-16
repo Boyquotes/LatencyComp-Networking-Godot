6c581c3b9d1390bb39b4181504696b8f87a0d64a
Vikram Peddinti <vikram.peddinti@gmail.com>
2021-03-15 20:44:52 -0700

    Basic Framework Cont. - Server + Client

--- Networking_Framework/menu_base.gd
+++ Networking_Framework/menu_base.gd
@@ -6,12 +6,41 @@ const SERVER_PORT = 5001
 const MAX_PLAYERS = 4
 var SERVER_IP = "127.0.0.1"
 
+# In order to have both a combination of Server and Client on one machine, 
+# there should be second SCENE TREE -> Server tree initialized
+# This will be the server function for EVERYONE, who will be connecting (including oneself)
+# Issue -> How will the physics work? This server is just going to be client prediction + lag conciliation
+# -> Attacks? Do we just use the main client as the baller?
+# https://godotengine.org/qa/89743/how-use-two-scenetrees-run-both-server-and-client-one-project
 
-func _custom_CreatePeerAsServer():
-    
-    #Create a new peer + start the server on the defined port
+
+func _ready():
+	get_tree().connect("network_peer_connected", self, "_player_connected")
+	#get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
+	get_tree().connect("connected_to_server", self, "_connected_to_server")
+	#get_tree().connect("connection_failed", self, "_connection_failed")
+	#get_tree().connect("server_disconnected", self, "_server_disconnected")
+
+func _player_connected(id):
+    print("player ",id," connected")
+
+func _connected_to_server():
+	print("_connected_to_server")
+	#rpc_id(1,"start_demo")
+	   
+#Create a new peer and start the SERVER on the defined SERVER_PORT with MAX_PLAYERS
+func _CreatePeerAsServer():
+    # Maybe pass in a tree, this will help with later?
     var peer = NetworkedMultiplayerENet.new()
     peer.create_server(SERVER_PORT, MAX_PLAYERS)
     get_tree().set_network_peer(peer)
-
+    print("Created Server")
     #Provide a Debug?
+
+#Create a new peer and start the CLIENT on the defined SERVER_PORT with MAX_PLAYERS
+func _CreatePeerAsClient():
+    var peer = NetworkedMultiplayerENet.new()
+    peer.create_client(SERVER_IP, SERVER_PORT)
+    get_tree().set_network_peer(peer)
+    print("Created Client")
+
